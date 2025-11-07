d4ec4a0aa1bd380217331005ba2b1f2b0f80af6c
git-youmesh <umesh.bomme.gowda@gmail.com>
2025-11-07 12:52:28 +0530

    Implement supervised fine-tuning for MNLI dataset

---
+++ SupervisedFineTuning.py
@@ -0,0 +1,75 @@
+from datasets import load_dataset,Dataset
+from sentence_transformers import SentenceTransformer
+from sentence_transformers import losses
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+# Load MNLI dataset from GLUE
+# 0 = entailment, 1 = neutral, 2 = contradiction
+train_dataset = load_dataset(
+ "glue", "mnli", split="train"
+).select(range(50_000))
+train_dataset = train_dataset.remove_columns("idx")
+
+embedding_model = SentenceTransformer("bert-base-uncased")
+mapping = {2: 0, 1: 0, 0:1}
+train_dataset = Dataset.from_dict({
+ "sentence1": train_dataset["premise"],
+ "sentence2": train_dataset["hypothesis"],
+ "label": [float(mapping[label]) for label in
+train_dataset["label"]]
+})
+
+print(train_dataset[2])
+
+
+evaluator = EmbeddingSimilarityEvaluator(
+ sentences1=val_sts["sentence1"],
+ sentences2=val_sts["sentence2"],
+ scores=[score/5 for score in val_sts["label"]],
+ main_similarity="cosine"
+)
+
+
+val_sts = load_dataset("glue", "stsb", split="validation")
+evaluator = EmbeddingSimilarityEvaluator(
+ sentences1=val_sts["sentence1"],
+ sentences2=val_sts["sentence2"],
+ scores=[score/5 for score in val_sts["label"]],
+ main_similarity="cosine"
+)
+train_loss = losses.SoftmaxLoss(
+model=embedding_model,
+sentence_embedding_dimension=embedding_model.get_sentence_embedding_dimension(),
+num_labels=3
+)
+
+
+args = SentenceTransformerTrainingArguments( 
+ output_dir="cosineloss_embedding_model",
+ num_train_epochs=1,
+ per_device_train_batch_size=32,
+ per_device_eval_batch_size=32,
+ warmup_steps=100,
+ fp16=True,
+ eval_steps=100,
+ logging_step=100
+)
+
+trainer = SentenceTransformerTrainer(
+ model=embedding_model,
+ args=args,
+ train_dataset=train_dataset,
+ loss=train_loss,
+ evaluator=evaluator
+)
+trainer.train()
+
+evaluator(embedding_model)
+
+
+
+
+
+
